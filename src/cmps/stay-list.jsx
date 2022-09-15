89da64b92d1334af834f10e644d161bf650069df
InbarSabag <pe.inbar@gmail.com>
2022-09-15 19:14:37 +0300

    preview set

--- src/cmps/stay-list.jsx
+++ src/cmps/stay-list.jsx
@@ -3,8 +3,9 @@ import { StayPreview } from './stay-preview'
 export function StayList({ stays, onRemoveStay, onAddReview }) {
     
     return (
-        <div className='robot-list simple-cards-grid'>
-            {stays.map(stay => <StayPreview onAddReview={onAddReview} key={stay._id} stay={stay} onRemoveStay={onRemoveStay}  />)}
-        </div>
+        <section className='stay-list simple-cards-grid'>
+            {stays.map(stay => 
+            <StayPreview onAddReview={onAddReview} key={stay._id} stay={stay} onRemoveStay={onRemoveStay}  />)}
+        </section>
     )
 }
